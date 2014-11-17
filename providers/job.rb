c0d20e53454ce21fba860e524ec52d1184c8995f
Michał Knapik <michal.knapik@u2i.com>
2014-11-17 11:12:52 +0100

    extract helpers

--- providers/job.rb
+++ providers/job.rb
@@ -1,23 +1,5 @@
 require 'securerandom'
 
-class ::Hash
-  def deep_merge!(other_hash, &block)
-    other_hash.each_pair do |k,v|
-      tv = self[k]
-      if tv.is_a?(Hash) && v.is_a?(Hash)
-        self[k] = tv.deep_merge(v, &block)
-      else
-        self[k] = block && tv ? block.call(k, tv, v) : v
-      end
-    end
-    self
-  end
-
-  def deep_merge(other_hash, &block)
-    dup.deep_merge!(other_hash, &block)
-  end
-end
-
 def whyrun_supported?
   true
 end
