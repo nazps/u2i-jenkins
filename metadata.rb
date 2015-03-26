0ebd2f2b6a7286dfcfd6a755a4c864b4c481f6f7
Michał Knapik <michal.knapik@u2i.com>
2015-03-26 09:52:30 +0000

    set mysql version to 5.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'michal.knapik@u2i.com'
 license          'All rights reserved'
 description      'Installs/Configures u2i-jenkins'
 long_description 'Installs/Configures u2i-jenkins'
-version          '0.4.3'
+version          '0.4.4'
 
 depends 'java', '~> 1.29.0'
 depends 'apache2', '~> 2.0.0'
